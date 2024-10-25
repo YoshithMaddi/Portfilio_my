82e5ba5d81a9b0cfd1fd67e4574d5d3d3ae1fde6
YoshithMaddi <yoshithmy23@gmail.com>
2024-10-25 13:13:04 +0530

    upto achievements section done

--- src/components/sub/Achievements.jsx
+++ src/components/sub/Achievements.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-
-const Achievements = () => {
+import {motion,useMotionValue} from 'framer-motion'
+const Achievements = ({title,amount,children}) => {
+  const number=useMotionValue(0)
+  const count =(amount)=>{
+    let i =0;
+    const updatecount =()=>{
+      let timeOut
+      if(i<=amount){
+        number.set(i++)
+        timeOut=setTimeout(updatecount,0);
+      }
+      else clearTimeout(timeOut)
+    }
+    updatecount()
+  }
   return (
     <div className='flex items-end gap-x-3'>
-        <span className='text-4xl lg:text-2xl text-gray-300 '>Icon</span>
+        <span className='text-4xl lg:text-2xl text-gray-300 '>{children}</span>
         <h1 className='flex flex-col gap-y-2'>
-            <span className='text-2xl lg:text-xl font-light text-yellow-500 '>348</span>
-            <span className='text-sm tracking-wide text-gray-500'>Github repo </span>
+            <motion.span className='text-2xl lg:text-xl font-light text-yellow-500 ' whileInView={()=>count(amount)} viewport={{once:true}}>{number}</motion.span>
+            <span className='text-sm tracking-wide text-gray-500'>{title}</span>
         </h1>
     </div>
   )
