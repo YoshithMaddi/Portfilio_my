0f0da59c272f2343acb301736cde7542a6ec09a5
YoshithMaddi <yoshithmy23@gmail.com>
2024-10-25 19:36:53 +0530

    doing skills page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 'use client'
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 import Hero from "@/components/Hero"
+import Skills from "@/components/Skills";
+import Achievements from "@/components/sub/Achievements";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
       <About/>
+      <Achievements></Achievements>
+      <Experience></Experience>
+      <Skills/>
     </div>
 
   );
---
+++ src/components/Skills.jsx
@@ -0,0 +1,19 @@
+'use client'
+import Heading from "./sub/Heading"
+import Image from "next/image"
+
+const Skills=()=>{
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-y-20 px-96">
+        <Heading text={'Skills'}/>
+        <div className="w-full flex justify-between flex-wrap gap-x-8 gap-y-10 lg:gap-y-6">
+            <div className="flex items-center justify-center gap-x-3 rounded-xl border border-yellow-500 bg-zinc-200 px-5 py-2 lg:px-2">
+                <Image src={'/skills/nextjs.png'} alt={"Next.js Logo"} width={100} height={100} className="h-auto w-[40px]"/>
+                <p className="text-sm text-gray-600">NextJS</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
